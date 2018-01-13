d88699f05d0879e8897b53e62686722012dfc33f
Gabriel Frederico Takahashi Vargas <33089095+GabrielTaka@users.noreply.github.com>
2018-01-12 22:04:00 -0200

    Add files via upload

---
+++ EP1/Gabriel Frederico Takahashi Vargas EP1.py
@@ -0,0 +1,79 @@
+txtB = '''clnr bcdktps fvxsmsbx kqj hvbncjw wsmngb xhcvvc nfkjfn fkvl ljhqlbhs ptqtwp
+vtfbq szkpmn hxl hsd qmrr jzjbjgp tsssrk fmvx bstbzwsx njdm nrvfgs bdjzlg nwnlmbx
+vjqqxsp vbxj gwtll xfrmgqqj mmttm xlbnkbw bkw frgwz twrkx nfgxqmb cvr tgmw nfw mghts
+bsncq zfn vpkgwcd cdbdwsjf krrkl fst lmz vvwtkrf dqt fkm pfnpqh vdslwsdk kbmfjgs fkwkbd
+nssd clbpzcpd qph ksfgcvw mcs nbjbkrz jbtcbqm lpj wvsscb tpqnm gswg gtd dpf shztl
+brgkfqnf xgw fsjmrvcx qzd slt xhhg vxgcfc hxfq jhxpngr nmpx gjdn kgmq hpb klzsxz
+bpcqrhq wrnn mgthn wzjcvj pgft vtksnpbb qtlmgsh nrzdkx lvd kpr fwrgdmjt jzknzgk vqmkgkb
+mptrq rslljk trggnlpd fcffvg wshnn tvq skddwrtg srd gbwdh pvgkrhd qndpq phmhxkck vkgfc
+mzpp gph nzn lcxc jpnx mxrg xpjkjxc fkhr fqfqcjtd bzmvqp wlsdx txlttnpb vdb mxnswm
+dwqnsgj mxg xmszj bdttl xmwth nfchzb vtlxg lqmxbx sgs hnw zgdsp qcgpc xhk pwbfdmtc
+ftkgv hqntlps dgbwpk jzsgkb kcsb xjnjhgh ckx pxzm tlpzlxj bjdd rjjp mxqxqdxc kfvg
+mqpvxk cmg jgz fmzf bnr fvfgnzx crkxcs zszmrfjv qsjgzzzp lcgsgjvh znjntxpj hdqzjc
+tmzxrrg nqlnsk mwtlm cvdk vqbhj wbpdssgm nsnv nhfptrsg vjgrlfs zkvkdxz brzbhlns pfs
+cvrjxjq fbbkvdhr bhtlqhvv rhjtsvv gfbrqn mvclz ghm sgkk tcmfz dtrmkn dzcjppjm mlpkx
+qbqbvpsm xxd vrqq sjwwc pchqk jnwp txk pvf dpff lqrdrz ncv mwgf chnz rjlfpch rnvdjpc
+mfxs shdf nfpnlr rvqw zmllbxs sfbvz hbcv bgp jtg bgsfnz hgkkwd nnzbqwgs lktdlrlx qxrs
+dpcj kwfj tjsh rnxhwgd dpmdnz xgnfggb pfrxglb plzxjqlk whts jgrt clvtxn mhnhb wndwxs
+wlkdtjz ghhmdq bcw vvlhntpt jnzznp bksx mvssxl kjdkt pjjzqkvx vwtbh rkkxqk xbwknvmr
+nmddl rnwqq frfbhk ctvfgxzv gplktxj lljfz lntr bndwhjwp tvr gbjz lbjrnmt hzjwqn wnxsmnx
+mjxh hlsssh nczkp wtbfv ztbcph cnhgxsd qddzdv ktzb jwhvvrtr qlrhnww zld cdvr xvsbdw
+lvsbxzv cslq cjvrwg sdvnw hcr stnhs nsxmr npqqm psr gcgg zpvrdnc qgzf qxp npfvr rzv
+ptkp drrlpnr dpptqzc vbf tdhps crjnct gvc fxt hckr vjhlfgld rfkvbr fbbhqvg kgzcf gsxm
+fvjkmj xmchrvwx dcxggwhc fmt jwvbhl ppldgl mfxmqjn tvcgnrb zssswpc ghhq dvsl vlj rqrsw
+pbvtkm kdtxsj qlgpv xtm rncp smnrwmlv kbndtscg gtps wbdxk qbm rqprpvj stgwsc lqklxr
+bxqhw hfhp lbrhbn klhtb wslrbz lpwqpn znsxfqs ldzzlkg hphjhns thc vddhjkq dpzrl mqt
+lvmptxcd bfxlg hnss bgncx slw rpgtkzz jgngwzc vjxgbs npdd hpzmp xfmnsjc scqskgsl dzxpk
+bwxr tbxwpk svtlgdl dzsvqbnx wjfktwv qtwhllw nndtsx hxmjpmnv cnxck cmh kccw hdfrtd
+qzcsksd dljps xcfstz rvfqp hrwx pslzhzhc bkwfwfnj phcxvpf lpwl dzftkjr vpvwnjtg srhnz
+hwlqvc dsrdq ntk vnxr vbmd kctx jhg ptfhnlc xxtctgsj pwj pxwt fzzx zzzfchrk qznnxl gdzj
+rjd pkj jlmwtcs twgzh ttmgnwb hwjbnqwv zgblvhlj bgg sqhfcnlk slkbcmj nwmm hgjjksxf
+drfrgjx hbjvpw rjrlrfk hcv frp rbsfrrcf jtkqhh kjtqpxhw hxhjznc jgxn dtlsp llw xvrvsdf
+cgfwq wrmv kcnr mpqnqr dqtswd qnnxhm jsl njxtbh zcbpcbpt bdllcsdl qbxwpg mlgtjw zbgsxg
+lbcxxgsf cbwjfldn rdp vjwsjpw srvc rkln bddvc gbgw nckdtf ckvmtbvf cwnf cnsmqxwn zvxhgq
+mgj pxv jms hbjr fpvxzwmx srlml rzfmdp xtxsblgt gwbvj krnn lthk dcx vhpbdwd rtvmzn
+bcclbzz hcdp qlw mxpwc lxccgcxh zhprp rsfpxl pmznqzh nbbkqjt cbmhp hbtn vlgfskcx cwh
+jtxhfvr jjc ttjcc cqlphsk mtgnc bdr xvpztf sxpb fxh gpgpqtrc pvpcmx wvcsmb lkd qxpm
+tdbxnwrg wvcpw hwswwqg bhkwfwxm cbvwhf bgnvwqln fgn ntnhcl ffq btzjd pbzqnc thfdcpxt
+rzmjrbfb lcrdlc tqlqrf ffkbj kqt qnkfd jwbt rgc zdbsvmll pfvnpwj ppq kltqfx klmjxg
+qpnnjwl xdvs qdvjskx dcnhhltm kqwxr xrzwj prmqclss xvhcb lfn hlcnqnw wbnxl nmjkkmpg gth
+gbb xsnhn qgjs xlt nmzhrrqn bkrhjtsl rvqzhmm lzlbc lmpfkk xtkdp ckm vhbnd kmhj xzrz
+gwdxkhr xnk jwnwz knbhrwgs dfcbw nfgkxsw fbg rntpnh mvkfdhh rcltszw lhld plr xhqdvhmp
+xhxsqp zqmsnl wkfxqzkx rdzhzx znzppsg hmqhxfpb nnmr kfpqcpx zbfck sdqxsnw cvl vsdrhj
+pbmlw gvp pwlhnpgf rrpzcwp pmcmrmvf fttbf zgtkjdm ddqmr twtksl vkdns rffn vkjnkkv
+hslbhksz glzb grwpg szzw rwmbvt grtkzrwh nwkjt tkhnb wbswcvbh mzmlgpp gggck sdcptlln
+gqz vpkpbsn nnw pnbqqbk mrgnflhr tchctgjn zmfkxvms kvqwc kgsh jfdjq mqndvm kckksgp 
+wrrdnmjz bjjcsvms dvfqjqsf wps ngrngr rspxz bnvkmhcl kxgdbxhh kbcbg dmzwnfgm qnmtrvx
+kqwjtrcg'''.split()
+
+
+
+#Pergunta A - Preposiçôes:
+
+listaNarutao = []
+
+for i in range(0,(len(txtB))):
+    if txtB[i].startswith(tuple('bct')) and txtB[i].endswith((tuple('bct'))):
+        listaNarutao.append(txtB[i])
+print (len(listaNarutao))
+
+
+#Pergunta B - Verbos:
+
+listaverbo = []
+
+for i in range(0,(len(txtB))):
+    if len(txtB[i])== 7:
+        if txtB[i].endswith((tuple('adefghijklçmnopqrsuvwxyz'))):
+            listaverbo.append(txtB[i])
+print (len(listaverbo))
+
+#Pergunta C - Primeira pessoa
+
+lista1 = []
+
+for i in range(0,(len(txtB))):
+    if txtB[i].startswith(tuple('adefghijklçmnopqrsuvwxyz')) and len(txtB[i])== 7:
+            if txtB[i].endswith((tuple('adefghijklçmnopqrsuvwxyz'))):
+                lista1.append(txtB[i])
+print (len(lista1))
